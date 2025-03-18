d8a44501aede4b196ca3fb1e05b1e8d0f866471c
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-18 11:07:54 +0100

    Agregar pantalla hernia abierta

---
+++ app/src/main/java/aplicacion/quirofano/HerniaAbiertaActivity.kt
@@ -0,0 +1,12 @@
+package aplicacion.quirofano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HerniaAbiertaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hernia_abierta)
+
+    }
+}
