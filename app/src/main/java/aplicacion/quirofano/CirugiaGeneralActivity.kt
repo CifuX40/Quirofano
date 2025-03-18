e8d46f9f7b5d6791214ec5f85de9050eb59d1c7f
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-18 10:44:36 +0100

    Agregar pantalla colecistectomia

--- app/src/main/java/aplicacion/quirofano/CirugiaGeneralActivity.kt
+++ app/src/main/java/aplicacion/quirofano/CirugiaGeneralActivity.kt
@@ -1,5 +1,6 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -8,5 +9,17 @@ class CirugiaGeneralActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cirugia_general)
+
+        val btnTiroidectomia = findViewById<Button>(R.id.button)
+        btnTiroidectomia.setOnClickListener {
+            val intent = Intent(this, TiroidectomiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnColecistectomia = findViewById<Button>(R.id.button2)
+        btnColecistectomia.setOnClickListener {
+            val intent = Intent(this, ColecistectomiaActivity::class.java)
+            startActivity(intent)
         }
     }
+}
