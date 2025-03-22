a9febb12b7ba431275517083a6defe81e682d7bc
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-22 11:50:54 +0100

    Botónes vascular

--- app/src/main/java/aplicacion/quirofano/CirugiaVascularActivity.kt
+++ app/src/main/java/aplicacion/quirofano/CirugiaVascularActivity.kt
@@ -1,11 +1,31 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CirugiaVascularActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cirugia_vascular)
+
+        val btnEndarterectomia = findViewById<Button>(R.id.buttonEndarterectomia)
+        btnEndarterectomia.setOnClickListener {
+            val intent = Intent(this, EndarterectomiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnVaricesConvencional = findViewById<Button>(R.id.buttonVaricesConvencional)
+        btnVaricesConvencional.setOnClickListener {
+            val intent = Intent(this, VaricesConvencionalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnVaricesRadiofrecuencia = findViewById<Button>(R.id.buttonVaricesRadiofrecuencia)
+        btnVaricesRadiofrecuencia.setOnClickListener {
+            val intent = Intent(this, VaricesRadiofrecuenciaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/aplicacion/quirofano/VaricesRadiofrecuenciaActivity.kt
@@ -0,0 +1,11 @@
+package aplicacion.quirofano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VaricesRadiofrecuenciaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.varices_radiofrecuencia)
+    }
+}
