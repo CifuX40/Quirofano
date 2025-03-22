ed621fd9dd79970261da622cc5d7c804c12de71a
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-22 11:00:27 +0100

    Botónes urología

--- app/src/main/java/aplicacion/quirofano/UrologiaActivity.kt
+++ app/src/main/java/aplicacion/quirofano/UrologiaActivity.kt
@@ -1,6 +1,8 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class UrologiaActivity : AppCompatActivity() {
@@ -8,5 +10,22 @@ class UrologiaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.urologia)
 
+        val btnCircuncision = findViewById<Button>(R.id.buttonCircuncision)
+        btnCircuncision.setOnClickListener {
+            val intent = Intent(this, CircuncisionActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnCistoscopia = findViewById<Button>(R.id.buttonCistoscopia)
+        btnCistoscopia.setOnClickListener {
+            val intent = Intent(this, CistoscopiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnVasectomia = findViewById<Button>(R.id.buttonVasectomia)
+        btnVasectomia.setOnClickListener {
+            val intent = Intent(this, VasectomiaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/aplicacion/quirofano/VasectomiaActivity.kt
@@ -0,0 +1,11 @@
+package aplicacion.quirofano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VasectomiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.vasectomia)
+    }
+}
