7c938d4bfe07f40a0d61d178d697f663b4e2115c
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-22 10:44:42 +0100

    Botónes cirugia plástica

--- app/src/main/java/aplicacion/quirofano/CirugiaPlasticaActivity.kt
+++ app/src/main/java/aplicacion/quirofano/CirugiaPlasticaActivity.kt
@@ -1,11 +1,25 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CirugiaPlasticaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cirugia_plastica)
+
+        val btnMastectomia = findViewById<Button>(R.id.buttonMastectomia)
+        btnMastectomia.setOnClickListener {
+            val intent = Intent(this, MastectomiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnProtesisMamaria = findViewById<Button>(R.id.buttonProtesisMamaria)
+        btnProtesisMamaria.setOnClickListener {
+            val intent = Intent(this, ProtesisMamariaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/aplicacion/quirofano/MastectomiaActivity.kt
@@ -0,0 +1,11 @@
+package aplicacion.quirofano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MastectomiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mastectomia)
+    }
+}
