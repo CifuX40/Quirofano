49f2b22222d0c0154d4afa09b96ede88ab19401b
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-22 10:25:18 +0100

    8.14-milestone-5 + tabulación

--- app/src/main/java/aplicacion/quirofano/UrologiaActivity.kt
+++ app/src/main/java/aplicacion/quirofano/UrologiaActivity.kt
@@ -1,7 +1,6 @@
 package aplicacion.quirofano
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class UrologiaActivity : AppCompatActivity() {
