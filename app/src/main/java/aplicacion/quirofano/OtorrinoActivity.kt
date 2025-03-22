ec4aa1f5e2aa44cdd9f0ea4f64c59c09a788d5e0
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-22 12:31:17 +0100

    Botónes Otorrino

--- app/src/main/java/aplicacion/quirofano/OtorrinoActivity.kt
+++ app/src/main/java/aplicacion/quirofano/OtorrinoActivity.kt
@@ -1,11 +1,37 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class OtorrinoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.otorrino)
+
+        val btnSeptoplastia = findViewById<Button>(R.id.buttonSeptoplastia)
+        btnSeptoplastia.setOnClickListener {
+            val intent = Intent(this, SeptoplastiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnFracturaNasal = findViewById<Button>(R.id.buttonFracturaNasal)
+        btnFracturaNasal.setOnClickListener {
+            val intent = Intent(this, FracturaNasalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnAdenoidectomia = findViewById<Button>(R.id.buttonAdenoidectomia)
+        btnAdenoidectomia.setOnClickListener {
+            val intent = Intent(this, AdenoidectomiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnTimpanoplastia = findViewById<Button>(R.id.buttonTimpanoplastia)
+        btnTimpanoplastia.setOnClickListener {
+            val intent = Intent(this, TimpanoplastiaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/aplicacion/quirofano/TimpanoplastiaActivity.kt
@@ -0,0 +1,11 @@
+package aplicacion.quirofano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TimpanoplastiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.timpanoplastia)
+    }
+}
