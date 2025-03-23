f30a9d037013b3dbafbdcb5d6513c4005e88f0b0
CifuX40 <carlos.cifuentes@live.u-tad.com>
2025-03-23 09:41:08 +0100

    Botónes ginecologia

--- app/src/main/java/aplicacion/quirofano/GinecologiaActivity.kt
+++ app/src/main/java/aplicacion/quirofano/GinecologiaActivity.kt
@@ -1,11 +1,19 @@
 package aplicacion.quirofano
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class GinecologiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ginecologia)
+
+        val buttonHisteroscopia: Button = findViewById(R.id.buttonHisteroscopia)
+        buttonHisteroscopia.setOnClickListener {
+            val intent = Intent(this, HisteroscopiaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
